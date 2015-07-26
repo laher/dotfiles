e49846f40a8fd1e0b8832125958f2e1e5c83c109
Am Laher <am.laher@hapara.com>
2015-07-26 19:56:42 +1200

    thefuck

--- setup-mac.sh
+++ setup-mac.sh
@@ -4,7 +4,8 @@ brew install zsh
 
 chsh -s $(which zsh)
 
-brew install git stow tmux wget
+brew install git stow tmux
 brew install macvim --with-cscope --with-lua --HEAD --override-system-vim
+brew install wget thefuck
 
 echo ":: Now do the manual setups from README.asciidoc"
--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -5,7 +5,8 @@ sudo apt-get install -y vim aptitude zsh
 chsh -s $(which zsh)
 
 sudo aptitude install -y git-core git chromium-browser \
-	stow tmux wget vim-nox
+	stow tmux wget vim-nox python-pip python-dev
+sudo pip install thefuck
 
 echo ":: Now do the manual setups from README.asciidoc"
 
