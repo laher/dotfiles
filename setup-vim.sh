87291fb1c27d3606303d7f11f8d11d0eae0c7dd1
Am Laher <laher@users.noreply.github.com>
2016-07-17 18:24:54 +1200

    setup vim-plug

--- setup-vim.sh
+++ setup-vim.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-DIR=~/.vim/bundle/Vundle.vim
-if [ ! -d "$DIR" ]; then
-	mkdir -p ~/.vim/bundle/
-	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-fi
-vim +PluginInstall +qall
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall
