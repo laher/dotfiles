dae170467c7abe75903c6a5712b61eaa5d589f45
Am Laher <laher@users.noreply.github.com>
2017-04-10 18:11:10 +1200

    neovim setup

--- setup-mac.sh
+++ setup-mac.sh
@@ -6,6 +6,7 @@ chsh -s $(which zsh)
 
 brew install git stow tmux
 brew install macvim --with-cscope --with-lua --HEAD --override-system-vim
+brew install neovim/neovim/neovim
 brew install wget thefuck
 
 echo ":: Now run ./vim-setup.sh and do the manual setups from README.asciidoc"
--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -7,6 +7,19 @@ chsh -s $(which zsh)
 sudo aptitude install -y git-core git \
 	stow tmux wget vim-nox python-pip python-dev curl
 sudo pip install thefuck
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install neovim
+sudo apt-get install python3-dev python3-pip
+
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+sudo update-alternatives --config vi
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --config vim
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+sudo update-alternatives --config editor
+
 
 echo ":: Now run ./setup-vim.sh and do the manual setups from README.asciidoc"
 
