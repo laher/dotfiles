0f8ff614fd7b092ee73bef7a81b0c59e90e27e86
Am Laher <am@movio.co>
2017-12-04 17:44:22 +1300

    udpate

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install -y vim aptitude zsh
-
-chsh -s $(which zsh)
-
-sudo aptitude install -y git-core git \
-	stow tmux wget vim-nox python-pip python-dev curl
-sudo pip install thefuck
-sudo apt-get install emacs
-git config --global credential.helper 'cache --timeout=3600'
+./setup-debian.sh
 
 #neovim has a ppa. debian?
 if grep --quiet Ubuntu /etc/lsb-release; then
