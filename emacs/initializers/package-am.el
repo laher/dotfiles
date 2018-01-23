fb3e7e5aede0650c42fdf03c9670e75548af7c79
Am Laher <am@movio.co>
2018-01-23 21:45:51 +1300

    udpate

--- emacs/initializers/package-am.el
+++ emacs/initializers/package-am.el
@@ -1,10 +1,16 @@
+;; See *-tokens.el (gitignored) for setting token with the following config section:
+;;
+;;  (use-package paradox
+;;  :ensure t
+;;  :defer t
+;;  :config
+;;   (require 'paradox)
+;;   (paradox-enable)
+;;   (setq paradox-github-token "....."))
+
 (use-package paradox
   :ensure t
-  :defer t
-  :config
-  (require 'paradox)
-  (paradox-enable)
-  (setq paradox-github-token ""))
+  :defer t)
 
 
 (use-package auto-package-update
