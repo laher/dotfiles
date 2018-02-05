083c1fd8d7000ff3aa63dc56955cbd2ddf89dc58
Am Laher <am@movio.co>
2018-02-05 13:09:50 +1300

    udpate

--- emacs/initializers/general-am.el
+++ emacs/initializers/general-am.el
@@ -13,6 +13,64 @@
   (golden-ratio-mode 1)
   (setq golden-ratio-auto-scale t))
 
+(use-package treemacs
+  :ensure t
+  :defer t
+  :config
+  (progn
+    (use-package treemacs-evil
+      :ensure t
+      :demand t)
+    (setq treemacs-change-root-without-asking nil
+          treemacs-collapse-dirs              (if (executable-find "python") 3 0)
+          treemacs-file-event-delay           5000
+          treemacs-follow-after-init          t
+          treemacs-follow-recenter-distance   0.1
+          treemacs-goto-tag-strategy          'refetch-index
+          treemacs-indentation                2
+          treemacs-indentation-string         " "
+          treemacs-is-never-other-window      nil
+          treemacs-never-persist              nil
+          treemacs-no-png-images              nil
+          treemacs-recenter-after-file-follow nil
+          treemacs-recenter-after-tag-follow  nil
+          treemacs-show-hidden-files          t
+          treemacs-silent-filewatch           nil
+          treemacs-silent-refresh             nil
+          treemacs-sorting                    'alphabetic-desc
+          treemacs-tag-follow-cleanup         t
+          treemacs-tag-follow-delay           1.5
+          treemacs-width                      35)
+
+    (with-eval-after-load 'winum
+      (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null (executable-find "python3"))))
+      (`(t . t)
+       (treemacs-git-mode 'extended))
+      (`(t . _)
+       (treemacs-git-mode 'simple))))
+  :bind
+  (:map global-map
+        ([f8]         . treemacs-toggle)
+        ("M-0"        . treemacs-select-window)
+        ("C-c 1"      . treemacs-delete-other-windows)
+        ("M-m ft"     . treemacs-toggle)
+        ("M-m fT"     . treemacs)
+        ("M-m fB"     . treemacs-bookmark)
+        ("M-m f C-t"  . treemacs-find-file)
+        ("M-m f M-t"  . treemacs-find-tag)))
+(use-package treemacs-projectile
+  :defer t
+  :ensure t
+  :config
+  (setq treemacs-header-function #'treemacs-projectile-create-header)
+  :bind (:map global-map
+              ("M-m fP" . treemacs-projectile)
+              ("M-m fp" . treemacs-projectile-toggle)))
 
 (provide 'general-am)
 ;;; general-am.el ends here
---
+++ emacs/initializers/js-am.el
@@ -0,0 +1,44 @@
+;;; js-am.el --- js settings
+;;
+;;; Commentary:
+;;
+;;; Settings related to js
+;;
+;;; Code:
+(defun my-setup-indent (n)
+  ;; web development
+  (setq-local coffee-tab-width n) ; coffeescript
+  (setq-local javascript-indent-level n) ; javascript-mode
+  (setq-local js-indent-level n) ; js-mode
+  (setq-local js2-basic-offset n) ; js2-mode, in latest js2-mode, it's alias of js-indent-level
+  (setq-local web-mode-markup-indent-offset n) ; web-mode, html tag in html file
+  (setq-local web-mode-css-indent-offset n) ; web-mode, css in html file
+  (setq-local web-mode-code-indent-offset n) ; web-mode, js code in html file
+  (setq-local css-indent-offset n) ; css-mode
+  (add-to-list 'web-mode-indentation-params '("lineup-args" . nil))
+  (add-to-list 'web-mode-indentation-params '("lineup-calls" . nil))
+  (add-to-list 'web-mode-indentation-params '("lineup-concats" . nil))
+  (add-to-list 'web-mode-indentation-params '("lineup-ternary" . nil))
+  )
+
+(defun my-style ()
+  (interactive)
+  (message "My personal code style!")
+  ;; use space instead of tab
+  (setq indent-tabs-mode nil)
+  ;; indent 2 spaces width
+  (my-setup-indent 2))
+
+(use-package rjsx-mode
+  :ensure t
+  :config
+  (add-hook 'rjsx-mode-hook 'my-style)
+  )
+
+(use-package flow-minor-mode
+  :ensure t
+  :config
+  (add-hook 'js2-mode-hook 'flow-minor-enable-automatically)
+  )
+
+(provide 'js-am)
