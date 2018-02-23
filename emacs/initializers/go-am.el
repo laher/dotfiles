668a606fb96d30e909d176ee33c91db9fec7998a
Am Laher <am@movio.co>
2018-02-23 21:57:07 +1300

    udpate

--- emacs/initializers/go-am.el
+++ emacs/initializers/go-am.el
@@ -30,6 +30,52 @@
         "oR" 'go-rename
         "ot" 'go-test
         )
+    (defun am/go-run-tests (args)
+        (interactive)
+        (save-selected-window
+          (async-shell-command (concat "go test " args))))
+    (defun am/go-run-package-tests ()
+        (interactive)
+        (am/go-run-tests ""))
+
+    (defun am/go-run-package-tests-nested ()
+        (interactive)
+        (am/go-run-tests "./..."))
+    (defun am/go-run-test-current-function ()
+        (interactive)
+        (if (string-match "_test\\.go" buffer-file-name)
+            (let ((test-method (if go-use-gocheck-for-testing
+                                   "-check.f"
+                                 "-run")))
+              (save-excursion
+                  (re-search-backward "^func[ ]+\\(([[:alnum:]]*?[ ]?[*]?[[:alnum:]]+)[ ]+\\)?\\(Test[[:alnum:]_]+\\)(.*)")
+                  (am/go-run-tests (concat test-method "='" (match-string-no-properties 2) "'"))))
+          (message "Must be in a _test.go file to run go-run-test-current-function")))
+
+    (defun am/go-run-test-current-suite ()
+        (interactive)
+        (if (string-match "_test\.go" buffer-file-name)
+            (if go-use-gocheck-for-testing
+                (save-excursion
+                    (re-search-backward "^func[ ]+\\(([[:alnum:]]*?[ ]?[*]?\\([[:alnum:]]+\\))[ ]+\\)?Test[[:alnum:]_]+(.*)")
+                    (am/go-run-tests (concat "-check.f='" (match-string-no-properties 2) "'")))
+              (message "Gocheck is needed to test the current suite"))
+          (message "Must be in a _test.go file to run go-test-current-suite")))
+
+    (defun am/go-run-main ()
+        (interactive)
+        (shell-command
+          (format "go run %s"
+                  (shell-quote-argument (buffer-file-name)))))
+
+    (evil-ex-define-cmd  "GoTest" 'am/go-run-package-tests)
+    (evil-ex-define-cmd  "GoTestFunc" 'am/go-run-test-current-function)
+    (evil-ex-define-cmd  "GoDef" 'go-guru-definition)
+    (evil-ex-define-cmd  "GoInfo" 'go-guru-describe)
+    (evil-ex-define-cmd  "GoReferrers" 'go-guru-referrers)
+    (evil-ex-define-cmd  "GoChannelPeers" 'go-guru-peers)
+
+    (setq gofmt-command "goimports")
     (progn
       (add-hook 'before-save-hook 'gofmt-before-save)
       )
