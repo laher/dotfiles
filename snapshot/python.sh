0dbc5f8d374f4d1d59eda50183b63be5a15428f3
Am Laher <am@laher.net.nz>
2019-05-31 06:54:17 +1200

    udpate

--- snapshot/python.sh
+++ snapshot/python.sh
@@ -1,3 +1,5 @@
 #!/bin/zsh
-python3 pip3.py > packages/python3-packages.txt
-python2 pip2.py > packages/python2-packages.txt
+
+cd "$(dirname "$0")"
+python3 pip3.py|sort > ../packages/python3-packages.txt
+python2 pip2.py|sort > ../packages/python2-packages.txt
