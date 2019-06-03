58702f4b61e34b2ca725473cfbe52a708bc08f42
Am Laher <am@movio.co>
2019-06-03 16:56:11 +1200

    udpate-mac

--- setup-mac.sh
+++ setup-mac.sh
@@ -5,8 +5,6 @@ brew install zsh
 chsh -s "$(which zsh)"
 
 brew install git stow tmux
-brew install macvim --with-cscope --with-lua --HEAD --override-system-vim
-brew install neovim/neovim/neovim
-brew install wget thefuck
+brew install wget
 
 echo ":: Now run ./setup-pt2.sh and do the manual setups from README.asciidoc"
