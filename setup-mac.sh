ecb87210dfe74b94f527d836c62b4bbc656d9759
Am Laher <amir.laher@tixtrack.com>
2022-06-07 16:23:16 +1200

    udpate

--- setup-mac.sh
+++ setup-mac.sh
@@ -9,4 +9,8 @@ brew install git tmux wget
 brew install awscli gh go golangci-lint lazygit firefoxpwa fzf mysql redis node the_silver_searcher tree
 brew install --cask inkscape authy alt-tab foxitreader kitty mysqlworkbench
 
-echo ":: Now run ./setup-pt2.sh and do the manual setups from README.asciidoc"
+# neovim package manager
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+
