4fe105eec1a4deddf6e3ebaeb42b77f8effea638
Amir Laher <amirlaher@MacBook-Pro.fritz.box>
2022-06-07 16:02:45 +1200

    udpate

--- setup-mac.sh
+++ setup-mac.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-brew install zsh 
+brew install zsh
 
 chsh -s "$(which zsh)"
 
-brew install git stow tmux
-brew install wget
+brew install neovim stow
+brew install git tmux wget
+brew install awscli gh go golangci-lint lazygit firefoxpwa fzf mysql redis node the_silver_searcher tree
+brew install --cask inkscape authy alt-tab foxitreader kitty mysqlworkbench
 
 echo ":: Now run ./setup-pt2.sh and do the manual setups from README.asciidoc"
