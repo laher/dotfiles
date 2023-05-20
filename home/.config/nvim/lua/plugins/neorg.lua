bf3efc009d801d98ad85068e6f2e05ebb7ebcb65
Am Laher <am@laher.net.nz>
2023-05-20 12:55:14 +1200

    tidyup

--- home/.config/nvim/lua/plugins/neorg.lua
+++ home/.config/nvim/lua/plugins/neorg.lua
@@ -2,21 +2,6 @@ return {
   {
     "nvim-neorg/neorg",
     build = ":Neorg sync-parsers",
-    -- config = function()
-    --   require("neorg").setup()
-    --   local neorg_callbacks = require("neorg.callbacks")
-    --   neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, keybinds)
-    --           -- Map all the below keybinds only when the "norg" mode is active
-    --           keybinds.map_event_to_mode("norg", {
-    --             n = { -- Bind keys in normal mode
-    --               --         { "<C-c>", "external.exec.block" },
-    --             },
-    --           }, {
-    --             silent = true,
-    --             noremap = true,
-    --           })
-    --   end)
-    -- end,
     keys = {
       { '<localleader>x', ':Neorg exec cursor<CR>', desc = 'Exec code block' },
       { '<leader>o', ':Neorg index<CR>', desc = 'Launch neorg' },
@@ -38,43 +23,6 @@ return {
         ["core.summary"] = {},
         ["core.journal"] = {
           config = {
-            -- toc_format = function(entries)
-            --   local months_text = {
-            --     "January",
-            --     "February",
-            --     "March",
-            --     "April",
-            --     "May",
-            --     "June",
-            --     "July",
-            --     "August",
-            --     "September",
-            --     "October",
-            --     "November",
-            --     "December",
-            --   }
-            --   -- Convert the entries into a certain format to be written
-            --   local output = {}
-            --   local current_year
-            --   local current_month
-            --   for _, entry in ipairs(entries) do
-            --     -- Don't print the year and month if they haven't changed
-            --     if not current_year or current_year < entry[1] then
-            --       current_year = entry[1]
-            --       table.insert(output, "* " .. current_year)
-            --     end
-            --     if not current_month or current_month < entry[2] then
-            --       current_month = entry[2]
-            --       table.insert(output, "** " .. months_text[current_month])
-            --     end
-            --
-            --     -- Prints the file link
-            --     print(vim.inspect(entry))
-            --     table.insert(output, entry[4] .. string.format("[%s]", entry[5]))
-            --   end
-            --
-            --   return output
-            -- end,
           },
         },
         ["core.concealer"] = {
@@ -117,19 +65,6 @@ return {
       ["external.exec"] = {
         config = {
 
-          -- search file
-    -- local neorg_callbacks = require("neorg.callbacks")
-    -- neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, keybinds)
-    --     -- Map all the below keybinds only when the "norg" mode is active
-    --     keybinds.map_event_to_mode("norg", {
-    --         n = { -- Bind keys in normal mode
-    --             { "<C-c>", "external.exec.view" },
-    --         },
-    --     }, {
-    --         silent = true,
-    --         noremap = true,
-    --     })
-    -- end)
         }
       },
     },
@@ -139,19 +74,5 @@ return {
     { "pysan3/neorg-templates", dependencies = { "L3MON4D3/LuaSnip" } }, -- ADD THIS LINE
     { dir = "~/dl/neorg-exec" },
   },
-  -- config = function()
-    -- local neorg_callbacks = require("neorg.callbacks")
-    -- neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, keybinds)
-    --     -- Map all the below keybinds only when the "norg" mode is active
-    --     keybinds.map_event_to_mode("norg", {
-    --         n = { -- Bind keys in normal mode
-    --             { "<C-c>", "external.exec.view" },
-    --         },
-    --     }, {
-    --         silent = true,
-    --         noremap = true,
-    --     })
-    -- end)
-  -- end,
 },
 }
